7b4ea55c3cfcb2aa0887ae601a09dcc1120ee170
arisbhakti <arisscalper@gmail.com>
2026-02-20 00:45:48 +0700

    hero section done

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Header() {
   return (
-    <div className="w-full h-20 box-border border-b border-neutral-800 flex flex-row items-center justify-between px-4 md:px-32 fixed z-40 bg-black">
+    <div className="fixed z-50 flex h-20 w-full flex-row items-center justify-between border-b border-neutral-800 bg-black px-4 box-border md:bg-transparent md:px-32">
       <div className="flex flex-row items-center justify-center gap-2.25 md:gap-2">
         <div className="w-6 md:w-10 border box-border border-white h-px"></div>
         <span className="text-primary-200 font-bold text-md md:text-xl leading-text-xl md:pl-2">
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import Image from "next/image";
 export default function Hero() {
   return (
-    <div className="relative z-30 bg-black">
-      <div className="relative mt-20 common-padding ">
+    <div
+      className="relative z-30 w-full bg-black md:h-[874px]
+    "
+    >
+      <div className="relative mt-0 common-padding ">
         <div className="w-px bg-neutral-800 absolute left-74 h-113 z-0 md:hidden"></div>
 
         {/* Hero Text , text & button */}
-        <div className="flex flex-col gap-10 py-10 z-10 md:gap-15 md:top-0 md:relative">
+        <div className="flex flex-col gap-10 py-10 md:py-0 z-40 md:gap-15 md:top-55.25 md:relative md:w-201.75">
           <div className="flex flex-col gap-4">
             {/* texts */}
             <div className="flex flex-row items-center gap-2 md:gap-4">
@@ -33,27 +36,29 @@ export default function Hero() {
       </div>
 
       <div
-        className="text-center relative bg-black h-128.25 md:top-0 md:right-0"
+        className="relative h-[513px] bg-black text-center md:absolute md:top-0 md:right-0 md:h-[881px] md:w-[661px] overflow-hidden "
         id="hero-div"
       >
         {/* green color */}
-        <div className="pointer-events-none absolute inset-y-0 left-1/2  w-1/2 bg-primary-200"></div>
+        <div className="pointer-events-none absolute inset-y-0 left-1/2  w-1/2   bg-primary-200"></div>
+
         <Image
-          src="/hero-image.svg"
+          src="/hero-image-desktop.svg"
           alt="Hero Image"
           width={392}
           height={466}
-          className="relative z-10 mx-auto -top-[3.9px] right-2 w-89.75 h-119 mix-blend-luminosity brightness-112 contrast-110 -rotate-2"
+          //   className="relative z-10 mx-auto -top-1 right-2 h-[476px] w-[359px] -rotate-2 mix-blend-luminosity brightness-110 contrast-110 md:absolute md:right-[30px] md:top-[66px] md:h-[872px] md:w-[658px]  md:brightness-115 md:contrast-110"
+          className="relative z-10 mx-auto -top-[3.9px] right-2 w-89.75 h-119 mix-blend-luminosity brightness-112 contrast-110 -rotate-2 md:absolute md:right-[20px]  md:h-[872px] md:w-[658px]  "
         />
 
-        {/* inset */}
+        {/* tint + fade */}
         <div className="pointer-events-none absolute inset-0 z-30 bg-black/20"></div>
-        <div className="pointer-events-none absolute inset-0 z-20 bg-[linear-gradient(to_bottom,rgba(0,0,0,0)_52%,rgba(0,0,0,0.72)_80%,#000000_100%)]"></div>
+        <div className="pointer-events-none absolute inset-0 z-20 bg-[linear-gradient(to_bottom,rgba(0,0,0,0)_52%,rgba(0,0,0,0.72)_80%,#000000_100%)] md:hidden"></div>
         <div className="absolute inset-0 z-20 pointer-events-none bg-linear-to-b from-black/40 to-black" />
 
         {/* Card Feedback */}
-        <div className="absolute w-87.25 top-[324.38px] px-4 py-4 bg-black flex flex-col gap-2 border border-neutral-800 border-box rounded-2xl mx-auto z-40 left-1/2 -translate-x-1/2 text-left items-start">
-          <div className="text-neutral-25 font-bold text-2xl leading-display-xs">
+        <div className="absolute w-87.25 md:w-79 top-[324.38px] md:top-153 px-4 py-4 md:px-5 md:py-5 bg-black flex flex-col gap-2 border border-neutral-800 border-box rounded-2xl mx-auto z-40 left-1/2 -translate-x-1/2 text-left items-start">
+          <div className="text-neutral-25 font-bold text-2xl leading-display-xs text-display-xl">
             5.0
           </div>
           <div className="flex flex-row items-center justify-center">
@@ -62,51 +67,56 @@ export default function Hero() {
               alt="Star"
               width={24}
               height={24}
-              className="inline-block"
+              className="inline-block md:w-8 md:h-8"
             />
             <Image
               src="/star-yellow.svg"
               alt="Star"
               width={24}
               height={24}
-              className="inline-block"
+              className="inline-block md:w-8 md:h-8"
             />
             <Image
               src="/star-yellow.svg"
               alt="Star"
               width={24}
               height={24}
-              className="inline-block"
+              className="inline-block md:w-8 md:h-8"
             />
             <Image
               src="/star-yellow.svg"
               alt="Star"
               width={24}
               height={24}
-              className="inline-block"
+              className="inline-block md:w-8 md:h-8"
             />
             <Image
               src="/star-yellow.svg"
               alt="Star"
               width={24}
               height={24}
-              className="inline-block"
+              className="inline-block md:w-8 md:h-8"
             />
           </div>
-          <div className="font-semibold leading-text-md text-text-md">
+          <div className="font-semibold leading-text-md text-text-md md:text-xl md:leading-text-lg">
             Many Client Trust with me
           </div>
         </div>
 
         {/* 3 boxes */}
-        <div className="absolute grid grid-cols-3 grid-rows-2 gap-0 top-[242.38px] z-30">
-          {/* atas: tengah */}
+        <div className="absolute top-60.5 z-30 grid grid-cols-3 grid-rows-2 gap-0 md:hidden">
           <div className="col-start-2 row-start-1 size-8.5 bg-primary-400" />
 
-          {/* bawah: kiri & kanan */}
           <div className="col-start-1 row-start-2 size-8.5 bg-primary-400" />
           <div className="col-start-3 row-start-2 size-8.5 bg-primary-400" />
         </div>
+      </div>
+
+      <div className="absolute bottom-0 z-30 md:grid grid-cols-3 grid-rows-2 gap-0 hidden ">
+        <div className="col-start-2 row-start-1 size-11.5 bg-primary-400" />
+
+        <div className="col-start-1 row-start-2 size-11.5 bg-primary-400" />
+        <div className="col-start-3 row-start-2 size-11.5 bg-primary-400" />
       </div>
     </div>
   );
