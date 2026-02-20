3093cdd09a49db44ebd6ead3448f791b8ce37d04
arisbhakti <arisscalper@gmail.com>
2026-02-20 21:17:06 +0700

    icons

--- components/Skiils.tsx
+++ components/Skiils.tsx
@@ -1,5 +1,17 @@
 import React from "react";
 import Image from "next/image";
+
+const skillsIcons = [
+  { src: "/icon-js.svg", alt: "js" },
+  { src: "/icon-css.svg", alt: "css" },
+  { src: "/icon-html.svg", alt: "html" },
+  { src: "/icon-express.svg", alt: "express" },
+  { src: "/icon-mongo.svg", alt: "mongo" },
+  { src: "/icon-react.svg", alt: "react" },
+  { src: "/icon-ts.svg", alt: "ts" },
+  { src: "/icon-docker.svg", alt: "docker" },
+];
+
 export default function Skiils() {
   return (
     <div className="common-padding flex flex-col gap-10">
@@ -15,82 +27,25 @@ export default function Skiils() {
           </h2>
         </div>
         {/* skills icons */}
-        <div className="grid grid-cols-4 gap-x-6 gap-y-6">
-          <div className="w-12 h-12 border box-border border-neutral-800 rounded-full flex items-center justify-center">
-            <Image
-              src="/icon-js.svg"
-              alt="HjsL"
-              width={38.4}
-              height={38.4}
-              className="mx-auto my-auto"
-            />
-          </div>
-          <div className="w-12 h-12 border box-border border-neutral-800 rounded-full flex items-center justify-center">
-            <Image
-              src="/icon-css.svg"
-              alt="css"
-              width={38.4}
-              height={38.4}
-              className="mx-auto my-auto"
-            />
-          </div>
-          <div className="w-12 h-12 border box-border border-neutral-800 rounded-full flex items-center justify-center">
-            <Image
-              src="/icon-html.svg"
-              alt="HTML"
-              width={38.4}
-              height={38.4}
-              className="mx-auto my-auto"
-            />
-          </div>
-          <div className="w-12 h-12 border box-border border-neutral-800 rounded-full flex items-center justify-center">
-            <Image
-              src="/icon-express.svg"
-              alt="express"
-              width={38.4}
-              height={38.4}
-              className="mx-auto my-auto"
-            />
-          </div>
-          <div className="w-12 h-12 border box-border border-neutral-800 rounded-full flex items-center justify-center">
-            <Image
-              src="/icon-mongo.svg"
-              alt="mongo"
-              width={38.4}
-              height={38.4}
-              className="mx-auto my-auto"
-            />
-          </div>
-          <div className="w-12 h-12 border box-border border-neutral-800 rounded-full flex items-center justify-center">
-            <Image
-              src="/icon-react.svg"
-              alt="react"
-              width={38.4}
-              height={38.4}
-              className="mx-auto my-auto"
-            />
-          </div>
-          <div className="w-12 h-12 border box-border border-neutral-800 rounded-full flex items-center justify-center">
-            <Image
-              src="/icon-ts.svg"
-              alt="ts"
-              width={38.4}
-              height={38.4}
-              className="mx-auto my-auto"
-            />
-          </div>
-          <div className="w-12 h-12 border box-border border-neutral-800 rounded-full flex items-center justify-center">
-            <Image
-              src="/icon-docker.svg"
-              alt="docker"
-              width={38.4}
-              height={38.4}
-              className="mx-auto my-auto"
-            />
-          </div>
+        <div className="flex w-66 flex-wrap gap-6">
+          {skillsIcons.map((icon) => (
+            <div
+              className="flex h-12 w-12  items-center justify-center rounded-full border border-neutral-800 box-border"
+              key={icon.src}
+            >
+              <Image
+                src={icon.src}
+                alt={icon.alt}
+                width={38.4}
+                height={38.4}
+                className="mx-auto my-auto"
+              />
+            </div>
+          ))}
         </div>
       </div>
       {/* Progress Skiils */}
+      <div id="progress-div"></div>
       <div></div>
     </div>
   );
