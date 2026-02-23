fd58e25dc340c547438ff5008004637b7a35ddbd
arisbhakti <arisscalper@gmail.com>
2026-02-23 15:26:42 +0700

    contact us done

--- components/ContactUs.tsx
+++ components/ContactUs.tsx
@@ -2,13 +2,13 @@ import React from "react";
 import Image from "next/image";
 export default function ContactUs() {
   return (
-    <div className="flex flex-col gap-0 w-full md:pb-30! pt-0 md:pt-20! relative md:flex-row md:px-32 md:py-20 md:gap-[122px]">
+    <div className="flex flex-col gap-0 w-full md:pb-30! pt-0 md:pt-20! relative md:flex-row md:px-32 md:py-20 md:gap-30.5 border-t border-t-neutral-800">
       {/* 3 boxes */}
-      <div className="absolute top-0 left-0 z-30 grid grid-cols-3 grid-rows-2 gap-0 md:hidden">
-        <div className="col-start-1 row-start-1 size-8.5 bg-primary-400" />
+      <div className="absolute top-0 left-0 z-30 grid grid-cols-3 grid-rows-2 gap-0">
+        <div className="col-start-1 row-start-1 size-8.5 md:size-11.5 bg-primary-400" />
 
-        <div className="col-start-2 row-start-2 size-8.5 bg-primary-400" />
-        <div className="col-start-3 row-start-1 size-8.5 bg-primary-400" />
+        <div className="col-start-2 row-start-2 size-8.5 md:size-11.5 bg-primary-400" />
+        <div className="col-start-3 row-start-1 size-8.5 md:size-11.5 bg-primary-400" />
       </div>
       {/* profile contact us */}
       <div className="w-full relative h-128.5  md:w-[420.89px] md:h-160 ">
@@ -24,7 +24,7 @@ export default function ContactUs() {
 
         {/* inset */}
         <div className="pointer-events-none absolute inset-0 z-30 bg-black/20"></div>
-        <div className="pointer-events-none absolute inset-0 z-20 bg-[linear-gradient(to_bottom,rgba(0,0,0,0)_50%,rgba(0,0,0,0.72)_80%,#000000_100%)] md:hidden"></div>
+        <div className="pointer-events-none absolute inset-0 z-20 bg-[linear-gradient(to_bottom,rgba(0,0,0,0)_50%,rgba(0,0,0,1)_80%,#000000_100%)] "></div>
         <div className="absolute inset-0 z-20 pointer-events-none bg-linear-to-b from-black/40 to-black" />
 
         {/* social media and profile name */}
@@ -90,7 +90,7 @@ export default function ContactUs() {
         </div>
       </div>
       {/* form contact us here */}
-      <div className="flex flex-col gap-6 px-4 py-10 h-158 realitive md:flex-1 md:gap-12">
+      <div className="flex flex-col gap-6 px-4 py-10 h-158 realitive md:flex-1 md:gap-12 md:h-full">
         {/* header */}
         <div className="flex flex-col gap-2">
           <h2 className="text-text-md leading-text-md font-medium text-primary-200 md:text-lg md:leading-text-lg">
@@ -148,11 +148,11 @@ export default function ContactUs() {
           <button className="w-full bg-primary-200 h-12 md:h-14 rounded-full text-neutral-950 font-extrabold text-sm md:text-text-md md:leading-text-md leading-text-sm shadow-[0_4px_40px_0_rgba(145,255,2,0.4)] hover:bg-primary-300 transition-colors duration-300 z-10 cursor-pointer">
             Send Message
           </button>
-          <div className="absolute bottom-0 right-0 z-0 grid grid-cols-3 grid-rows-2 gap-0 md:hidden">
-            <div className="col-start-2 row-start-1 size-8.5 bg-primary-400" />
+          <div className="absolute bottom-0 right-0 z-0 grid grid-cols-3 grid-rows-2 gap-0 ">
+            <div className="col-start-2 row-start-1 size-8.5 md:size-11.5 bg-primary-400" />
 
-            <div className="col-start-1 row-start-2 size-8.5 bg-primary-400" />
-            <div className="col-start-3 row-start-2 size-8.5 bg-primary-400" />
+            <div className="col-start-1 row-start-2 size-8.5 md:size-11.5 bg-primary-400" />
+            <div className="col-start-3 row-start-2 size-8.5 md:size-11.5 bg-primary-400" />
           </div>
         </div>
       </div>
